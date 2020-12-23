96aea46760ad482c8c7ceb1a226a4624049e0695
SS1DHoT <75005813+SS1DHoT@users.noreply.github.com>
2020-12-23 15:45:46 +0900

    Create request5.php

---
+++ request5.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+//ログイン済みの場合
+if (isset($_SESSION['USER'])) {
+  $user = $_SESSION['USER'];
+// データーベースへpdoで接続
+$pdo = new pdo("mysql:host=localhost;dbname=webapp","root","");
+
+header("Content-type: text/plain; charset=UTF-8");
+$cate = $_POST['cate'];
+
+	
+$stmt = $pdo->prepare('SELECT * FROM tubuyaki WHERE category=:cate');
+//変数を文字列に
+$stmt->bindValue(':cate', $cate, PDO::PARAM_STR);
+// クエリ実行
+$stmt->execute();
+
+$all = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($all);
+
+}
