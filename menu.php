5badb1c47f66e1a036e3c645c4f21f402eb05b73
Kamitsure11 <74891839+Kamitsure11@users.noreply.github.com>
2020-12-24 10:31:05 +0900

    Update menu.php
    デザイン実装

--- menu.php
+++ menu.php
@@ -5,25 +5,61 @@ session_start();
 <html lang="ja">
 <head>
 <meta charset="utf-8">
+<!--スマホサイズに合わせる-->
+    <meta name="viewport"
+    content="width=320,
+    height=480,
+    initial-scale=1.0,
+    minimum-scale=1.0,
+    maximum-scale=2.0,
+    user-scalable=yes" />
+<link rel="stylesheet" href="home.css">
 </head>
 <body>
-<?php
-if (isset($_SESSION['USER']) && $_SESSION['USER'] != null){
-?>
+
 <!--//感情整理画面へ遷移-->
-<input type="button" onclick="location.href='./emo_main.php'" value="感情整理">
+<!--<input type="button" onclick="location.href='./emo_main.php'" value="感情整理">-->
 <!--//記録画面へ遷移-->
-<input type="button" onclick="location.href='./emo_log.html'" value="記録">
+<!--<input type="button" onclick="location.href='./emo_log.html'" value="記録">-->
 <!--//心療内科紹介画面へ遷移-->
-<input type="button" onclick="location.href='./info.html'" value="心療内科紹介">
+<!--<input type="button" onclick="location.href='./info.html'" value="心療内科紹介">-->
 <!--//経験談表示画面へ遷移-->
-<input type="button" onclick="location.href='./timeline.html'" value="経験談">	
-<?php
-}else{
-    session_destroy();
-    print "<P>ちゃんとログオンしてね！<BR>
-           <A HREF='g_logon.html'>ログオン</A></P>";
-}
-?>
-	</body>
+<!--<input type="button" onclick="location.href='./timeline.html'" value="経験談">	-->
+
+	 <div>
+        <h1>ホーム</h1>
+    <form>
+        <div class="box">
+        <div class="card-1">
+        <div  onclick="location.href='./emo_main.php'" class="content-img1">
+        <img src="cocoro.png" />
+        </div>
+        <p class="content-1">メモ</p>
+        </div>
+        
+        <div class="card-2">
+        <div onclick="location.href='./emo_log.html'" class="content-img1">
+        <img src="graf.png" />
+        </div>
+        <p class="content-2">レコード</p>
+        </div>
+        
+        <div class="card-3">
+        <div onclick="location.href='./info.html'" class="content-img2">
+        <img src="soudan.png" />
+        </div>
+        <p class="content-3">クリニック</p>
+        </div>
+        
+        <div class="card-4">
+        <div onclick="location.href='./timeline.html'" class="content-img3">
+        <img src="keiji.png" />
+        </div>
+        <p class="content-4">メッセージ</p>
+        </div>
+        </div>
+    </form>
+    </div>
+    
+    </body>
 </html>
