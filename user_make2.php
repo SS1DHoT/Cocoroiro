6dfe9f7556bdb3e6dba41e515327fac43ffcba2f
Kamitsure11 <74891839+Kamitsure11@users.noreply.github.com>
2020-12-24 10:59:15 +0900

    Update user_make2.php
    デザイン実装

--- user_make2.php
+++ user_make2.php
@@ -6,8 +6,17 @@ $p = htmlspecialchars($_POST['password'],ENT_QUOTES);
 require_once("db_init.php");
 ?>
 <HTML lang="ja">
+<link rel="stylesheet" href="pin2.css">
 <HEAD>
 <META HTTP EQUIV='Content-Type' CONTENT='text/html;charset=UTF-8'>
+     <!--スマホサイズに合わせる-->
+    <meta name="viewport"
+    content="width=320,
+    height=480,
+    initial-scale=1.0,
+    minimum-scale=1.0,
+    maximum-scale=2.0,
+    user-scalable=yes" />
 <TITLE>こころの色</TITLE>
 </HEAD>
 <BODY>
@@ -15,7 +24,7 @@ require_once("db_init.php");
 
 // データーベースへpdoで接続
 $pdo = new pdo("mysql:host=localhost;dbname=webapp","root","");
-    
+
 // 接続に失敗すれば強制終了
 if(mysqli_connect_error()){
 	die("Failed Connect DB.");
@@ -38,11 +47,11 @@ if($_POST['user'] == ''){
 }elseif($_POST['password']==''){
     echo "Passwordを入力してください";
 }elseif ($out === 1){
-	
+
 ?>
 <P>そのユーザー名は既に使われています<BR>
 <A HREF='user_make.html'>新規登録画面</A></P>
-	
+
 <?php
 }else{
 	 $pdo->query("INSERT INTO user (name,pass) VALUES('$u',md5('$p'))");
