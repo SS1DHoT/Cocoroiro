c5036b00e3ceadd9e8022cd008edf45762b88c85
rikuya31 <74891698+rikuya31@users.noreply.github.com>
2021-01-20 14:14:58 +0900

    Update logon2.php

--- logon2.php
+++ logon2.php
@@ -4,34 +4,31 @@ session_start();
 $u = htmlspecialchars($_POST['user'],ENT_QUOTES);
 $p = htmlspecialchars($_POST['pass'],ENT_QUOTES);
 require_once("db_init.php");
-?>
 
-<HTML lang="ja">
-<HEAD>
-<META HTTP EQUIV='Content-Type' CONTENT='text/html;charset=UTF-8'>
-<TITLE>こころの色</TITLE>
-</HEAD>
-<BODY>
-
-<?php
 // データーベースへpdoで接続
-$pdo = new pdo("mysql:host=localhost;dbname=webapp","root","");
+$pdo = new pdo("mysql:host=mysql148.phy.lolipop.lan;dbname=LAA1210934-webapp","LAA1210934","12345");
     
 // 接続に失敗すれば強制終了
 if(mysqli_connect_error()){
 	die("Failed Connect DB.");
 }
 
-$ps = $pdo->query("SELECT pass FROM user WHERE name='$u'");
+$ps = $pdo->query("SELECT password FROM userdata WHERE user='$u'");
 if ($ps->rowCount()>0)
 {     $r = $ps->fetch();
-     if ($r['pass'] === md5($p)){
+     if ($r['password'] === md5($p)){
          $_SESSION['USER']= $u;
 		  header( "Location: ./menu.php" ) ;
 
     }else{
          session_destroy();
 ?>
+<HTML lang="ja">
+<HEAD>
+<META HTTP EQUIV='Content-Type' CONTENT='text/html;charset=UTF-8'>
+<TITLE>こころの色</TITLE>
+</HEAD>
+<BODY>
 <P>パスワードが違います<BR>
 <A HREF='logon.html'>ログイン画面へ</A></P>
 
