dc6aa2f61aba5e6b047ae4b5ca87dfc74440a81d
rikuya31 <74891698+rikuya31@users.noreply.github.com>
2021-01-20 14:17:15 +0900

    Update menu.php

--- menu.php
+++ menu.php
@@ -4,6 +4,14 @@ session_start();
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+<!-- アドレスバー等のブラウザのUIを非表示 -->
+<meta name="apple-mobile-web-app-capable" content="yes">
+<!-- default（Safariと同じ） / black（黒） / black-translucent（ステータスバーをコンテンツに含める） -->
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<!-- ホーム画面に表示されるアプリ名 -->
+<meta name="apple-mobile-web-app-title" content="ここいろ">
+<!-- ホーム画面に表示されるアプリアイコン -->
+<link rel="apple-touch-icon" href="images/icons/icon-144x144.png">
 <meta charset="utf-8">
 <!--スマホサイズに合わせる-->
     <meta name="viewport"
@@ -20,15 +28,15 @@ session_start();
 <!--//感情整理画面へ遷移-->
 <!--<input type="button" onclick="location.href='./emo_main.php'" value="感情整理">-->
 <!--//記録画面へ遷移-->
-<!--<input type="button" onclick="location.href='./emo_log.html'" value="記録">-->
+<!--<input type="button" onclick="location.href='./emo_log.php'" value="記録">-->
 <!--//心療内科紹介画面へ遷移-->
 <!--<input type="button" onclick="location.href='./info.html'" value="心療内科紹介">-->
 <!--//経験談表示画面へ遷移-->
-<!--<input type="button" onclick="location.href='./timeline.html'" value="経験談">	-->
+<!--<input type="button" onclick="location.href='./timeline.php'" value="経験談">	-->
 
 	 <div>
          <div align="center">
-             <img class="img1" src="sikikantouka.png"></div>
+             <img class="img1" src="./sikikantouka.png"></div>
     
     <form>
         <div class="box">
@@ -40,7 +48,7 @@ session_start();
         </div>
         
         <div class="card-2">
-        <div onclick="location.href='./emo_log.html'" class="content-img1">
+        <div onclick="location.href='./emo_log.php'" class="content-img1">
         <img src="graf.png" />
         </div>
         <p class="content-2">レコード</p>
@@ -54,7 +62,7 @@ session_start();
         </div>
         
         <div class="card-4">
-        <div onclick="location.href='./timeline.html'" class="content-img3">
+        <div onclick="location.href='./timeline.php'" class="content-img3">
         <img src="keiji.png" />
         </div>
         <p class="content-4">メッセージ</p>
